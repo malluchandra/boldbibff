94ad14aede9e685f62e35b9cc2130adf1f5cf5c7
Chandra SekharMallu <chandra.sekharmallu@koerber-supplychain.com>
2024-12-19 10:28:43 -0500

    Initial commit

---
+++ src/test/java/com/boldbi/proxy/boldbibff/BoldbibffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boldbi.proxy.boldbibff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoldbibffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
